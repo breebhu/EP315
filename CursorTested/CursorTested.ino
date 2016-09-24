d1b9f8be2816cec6ba5fdb55f0cc0fa178fdbab2
Archit <bhatnagar_archit@yahoo.co.in>
2016-09-25 03:04:10 +0530

    Cursor Done

---
+++ CursorTested/CursorTested.ino
@@ -0,0 +1,99 @@
+#include<openGLCD.h>
+#include<math.h>
+
+// int buttonPin = ;
+int joyPin1 = A5;                 // slider variable connecetd to analog pin 0
+int joyPin2 = A4;
+int SWPin = 3;
+// slider variable connecetd to analog pin 1
+int value1 = 0;                  // variable to read the value from the analog pin 0
+int value2 = 0;
+int value3 = -1;
+bool penLift = false;
+// variable to read the value from the analog pin 1
+int x, y;
+int real_image[3][3] = {0};
+
+void setup() {
+  GLCD.Init();
+  x = GLCD.CenterX; y = GLCD.CenterY;
+  real_image[1][1] = 1;
+  GLCD.ClearScreen();
+  GLCD.CursorTo(x, y);
+  pinMode(joyPin1, INPUT);
+  pinMode(joyPin2, INPUT);
+  pinMode(SWPin, INPUT);
+  digitalWrite(SWPin, HIGH);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  delay(150);
+  value1 = analogRead(joyPin1);
+  delay(10);
+  value2 = analogRead(joyPin2);
+  value3 = digitalRead(SWPin);
+
+  GLCD.SetDot((x - 1) % 128, (y - 1) % 64, real_image[0][0]);
+  GLCD.SetDot((x - 1) % 128, y, real_image[0][1]);
+  GLCD.SetDot((x - 1) % 128, (y + 1) % 64, real_image[0][2]);
+  GLCD.SetDot(x, (y - 1) % 64, real_image[1][0]);
+  GLCD.SetDot(x, y, real_image[1][1]); //May not need this
+  GLCD.SetDot(x, (y + 1) % 64, real_image[1][2]);
+  GLCD.SetDot((x + 1) % 128, (y - 1) % 64, real_image[2][0]);
+  GLCD.SetDot((x + 1) % 128, y, real_image[2][1]);
+  GLCD.SetDot((x + 1) % 128, (y + 1) % 64, real_image[2][2]);
+
+  if (value2 > 985)
+  {
+    x = x + 1;
+    if (x > 127)x = 0;
+  }
+  else if (value2 < 10)
+  {
+    x = x - 1;
+    if (x < 0)x = 127;
+  }
+  if (value1 < 10)
+  {
+    y = y + 1;
+    if (y > 63)y = 0;
+  }
+  else if (value1 > 985)
+  {
+    y = y - 1;
+    if (y < 0)y = 63;
+  }
+  if (value3 == 0)
+  {
+    penLift = !(penLift);
+  }
+
+  GLCD.SetDot(x, y, BLACK);
+
+  real_image[0][0] = ActualReadData((x - 1) % 128, (y - 1) % 64);
+  real_image[1][0] = ActualReadData(x, (y - 1) % 64);
+  real_image[2][0] = ActualReadData((x + 1) % 128, (y - 1) % 64);
+  real_image[0][1] = ActualReadData((x - 1) % 128, y);
+  real_image[1][1] = ActualReadData(x, y);
+  real_image[2][1] = ActualReadData((x + 1) % 128, y);
+  real_image[0][2] = ActualReadData((x - 1) % 128, (y + 1) % 64);
+  real_image[1][2] = ActualReadData(x, (y + 1) % 64);
+  real_image[2][2] = ActualReadData((x + 1) % 128, (y + 1) % 64);
+
+  GLCD.SetDot(x, y, BLACK);
+  GLCD.SetDot(x + 1, y, BLACK);
+  GLCD.SetDot(x - 1, y, BLACK);
+  GLCD.SetDot(x, (y + 1) % 64, BLACK);
+  GLCD.SetDot(x, y - 1, BLACK);
+
+}
+
+int ActualReadData(int x, int y) {
+  uint8_t bitarray[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
+  GLCD.GotoXY(x, y);
+  uint8_t data = GLCD.ReadData();
+  if (data & bitarray[y % 8] == bitarray[y % 8]) return BLACK;
+  else return WHITE;
+}
--- Paint_Test1/Paint_Test1.ino
+++ Paint_Test1/Paint_Test1.ino
@@ -1,88 +1,94 @@
 #include<openGLCD.h>
 
-
 // int buttonPin = ;
- int joyPin1 = A5;                 // slider variable connecetd to analog pin 0
- int joyPin2 = A4;
- int SWPin=3; 
- // slider variable connecetd to analog pin 1
- int value1 = 0;                  // variable to read the value from the analog pin 0
- int value2 = 0;   
- int value3 = -1;
- bool penLift=false;
- // variable to read the value from the analog pin 1
- bool up=false,down=false,left=false,right=false;
- int t_up=1000,t_down=10,t_right=1000,t_left=10;
- int x=0,y=0;
- int cursor_x = 0,cursor_y = 0;
- void setup() {
+int joyPin1 = A5;                 // slider variable connecetd to analog pin 0
+int joyPin2 = A4;
+int SWPin = 3;
+// slider variable connecetd to analog pin 1
+int value1 = 0;                  // variable to read the value from the analog pin 0
+int value2 = 0;
+int value3 = -1;
+bool penLift = false;
+// variable to read the value from the analog pin 1
+bool up = false, down = false, left = false, right = false;
+int t_up = 1000, t_down = 10, t_right = 1000, t_left = 10;
+int x, x1, x2, y, y1, y2;
+int cursor_x = 0, cursor_y = 0;
+void setup() {
   GLCD.Init();
+  x = GLCD.CenterX; y = GLCD.CenterY;
+  x1 = GLCD.CenterX; y1 = GLCD.CenterY;
+  x2 = GLCD.CenterX; y2 = GLCD.CenterY;
+  cursor_x = GLCD.CenterX; cursor_y = GLCD.CenterY;
   GLCD.ClearScreen();
-  GLCD.SelectFont(cp437font8x8);
-  GLCD.CursorTo(x,y);
-  pinMode(joyPin1,INPUT);
-  pinMode(joyPin2,INPUT);
-  pinMode(SWPin,INPUT);
-  digitalWrite(SWPin,HIGH);
+  GLCD.CursorTo(x, y);
+  pinMode(joyPin1, INPUT);
+  pinMode(joyPin2, INPUT);
+  pinMode(SWPin, INPUT);
+  digitalWrite(SWPin, HIGH);
   Serial.begin(9600);
- }
+}
 
- void loop() {
-  int prev_cur_x = cursor_x, prev_cur_y = cursor_y;
+void loop() {
+  x2 = x1; y2 = y1;
+  x1 = x; y1 = y;
   delay(50);
-  value1 = analogRead(joyPin1);  
-  delay(10);            
+  value1 = analogRead(joyPin1);
+  delay(10);
   value2 = analogRead(joyPin2);
-  value3 = digitalRead(SWPin); 
-  if(value1>985)
+  value3 = digitalRead(SWPin);
+  if (value2 > 985)
   {
-    right=false;
-    x=x+1;
-    if(x>127)x=0;
+    right = false;
+    x = x + 1;
+    if (x > 127)x = 0;
   }
-  else if(value1<10)
+  else if (value2 < 10)
   {
-    right=false;
-    x=x-1;
-    if(x<0)x=127;
+    right = true;
+    x = x - 1;
+    if (x < 0)x = 127;
   }
-  if(value2<10)
+  if (value1 < 10)
   {
-    up=true;
-    y=y+1;
-    if(y>63)y=0;
+    up = true;
+    y = y + 1;
+    if (y > 63)y = 0;
   }
-  else if(value2>985)
+  else if (value1 > 985)
   {
-    up=false;
-    y=y-1;
-    if(y<0)y=63;
+    up = false;
+    y = y - 1;
+    if (y < 0)y = 63;
   }
-  if(value3==0)
+  if (value3 == 0)
   {
-    penLift=!(penLift);
+    penLift = !(penLift);
   }
-  
+  GLCD.SetDot(cursor_x, cursor_y, WHITE);
+  GLCD.SetDot(cursor_x + 1, cursor_y, WHITE);
+  GLCD.SetDot(cursor_x - 1, cursor_y, WHITE);
+  GLCD.SetDot(cursor_x, cursor_y + 1, WHITE);
+  GLCD.SetDot(cursor_x, cursor_y - 1, WHITE);
+
   cursor_x = x; cursor_y = y;
-  GLCD.SetDot(prev_cur_x,prev_cur_y,WHITE);
-  GLCD.SetDot(prev_cur_x+1,prev_cur_y,WHITE);
-  GLCD.SetDot(prev_cur_x-1,prev_cur_y,WHITE);
-  GLCD.SetDot(prev_cur_x,prev_cur_y+1,WHITE);
-  GLCD.SetDot(prev_cur_x,prev_cur_y-1,WHITE);
+  GLCD.SetDot(cursor_x, cursor_y, BLACK);
+  GLCD.SetDot(cursor_x + 1, cursor_y, BLACK);
+  GLCD.SetDot(cursor_x - 1, cursor_y, BLACK);
+  GLCD.SetDot(cursor_x, cursor_y + 1, BLACK);
+  GLCD.SetDot(cursor_x, cursor_y - 1, BLACK);
 
-  GLCD.SetDot(cursor_x,cursor_y,BLACK);
-  GLCD.SetDot(cursor_x+1,cursor_y,BLACK);
-  GLCD.SetDot(cursor_x-1,cursor_y,BLACK);
-  GLCD.SetDot(cursor_x,cursor_y+1,BLACK);
-  GLCD.SetDot(cursor_x,cursor_y-1,BLACK);
-
-  if(!penLift)
-  GLCD.SetDot(x,y,BLACK);
-  else 
-  GLCD.SetDot(x,y,WHITE);
+  //if(!penLift)
+  GLCD.SetDot(x, y, BLACK);
+  GLCD.SetDot(x1, y1, BLACK);
+  GLCD.SetDot(x2, y2, BLACK);
+  //else
+  //GLCD.SetDot(x,y,WHITE);
   Serial.print(value1);
   Serial.print(",");
   Serial.print(value2);
   Serial.print(",");
-  Serial.println((value3));
- }
+  Serial.println(value3);
+  // Serial.print(",");
+  //  Serial.println(cursor);
+}
